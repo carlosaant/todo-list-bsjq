49e1b889aa2893bc8d165c0de6c9d47eb7b4518d
Antonio Carlos <carlos_antonio.minga@outlook.com>
2022-11-01 15:02:22 -0300

    estrutura JS com jquery botão

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
+let _tarefas = [];
+const _tarefa = {
+  descricao: '',
+  id_categoria: ''
+};
+
 document.addEventListener('DOMContentLoaded', function (event) {
   $('#button-add-tarefa').click(() => {
-    let texto_tarefa = $('#inp_text_tarefa').val();
-    let value_categoria = $('#inp_sel_categoria').val();
-    console.log(texto_tarefa, value_categoria);
-    // alert('Clicou!');
+    let texto_tarefa = $('#inp_text_tarefa').val().trim();
+    let value_categoria = $('#inp_sel_categoria').val().trim();
+    _tarefa.descricao = texto_tarefa;
+    _tarefa.id_categoria = value_categoria;
+    _tarefas.unshift(_tarefa);
+
+    console.log(_tarefas);
   });
 });
+
+function setLocalSt(arrTarefas) {
+  localStorage.setItem('tarefas-todo', JSON.stringify(arrTarefas));
+}
